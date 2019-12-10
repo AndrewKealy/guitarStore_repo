d0ebe3b91cb5945071a85f1355188f80c319c305
Ubuntu <ubuntu@ip-172-31-13-172.eu-west-1.compute.internal>
2019-12-10 01:42:32 +0000

    Second commit with orders

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -53,5 +53,22 @@ class CartController < ApplicationController
     end
   end
   
+  
+  def createOrder
+    @user =User.find(current_user.id)
+    @order =@user.orders.build(:order_date => DateTime.now, :status => 'Pending')
+    @order.save
+    
+    @cart = session[:cart]|| {}
+    @cart.each do |id, quantity|
+      item =Item.find_by_id(id)
+      @orderitem = @order.orderitems.build(:item_id => item.id, :title => item.title, :description => item.description, :quantity => quantity, :price => item.price)
+      @orderitem.save
+    
+    end
+    @orders = Order.last
+    @orderitems = Orderitem.where(order_id: Order.last)
+    session[:cart] = nil
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,33 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root  'static_pages#home'
+  resources :orders do 
+    resources :orderitems
+  end
+  
+  
+  
+  devise_for :users do
+    resources :orders
+  end  
+  
+  get 'checkout' => 'cart#createOrder'
   get 'cart/index'
 
   resources :items
-
-
+  get '/login' => 'user#login'
+  get '/logout' => 'user#logout'
+  
+  
   get '/help' => 'static_pages#help'
 
   get '/about' => 'static_pages#about'
   
-  get '/cart/:id', to: 'cart#add'
+  get '/cart/:id', to: 'cart#add' 
   
   get '/cart/remove/:id' => 'cart#remove'
+  get '/cart/decrease/:id' => 'cart#decrease'
 
-  get 'cart/decrease/:id' => 'cart#decrease'
-  
-   
+  root :to => 'site#home'
 
-   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
